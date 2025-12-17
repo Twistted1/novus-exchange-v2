253e6137002b833a4d5ed83385b51ec53ee85664
Marcio <putheworkin@gmail.com>
2025-12-17 01:01:12 +0000

    Clarify ESLint ignoreDuringBuilds comment
    Updated comment to clarify ESLint error handling during builds.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // This forces the build to finish even if "Grammar Police" finds errors
     ignoreDuringBuilds: true,
   },
 };
 
 module.exports = nextConfig;
-
