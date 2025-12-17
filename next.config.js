9c547742b7cc98e6c1afd20b3fb7f6b9e82cab14
Marcio <putheworkin@gmail.com>
2025-12-17 00:47:24 +0000

    Update next.config.js to configure ESLint
    Removed image remote patterns and added ESLint configuration.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'novusexchange.com' }
-    ]
-  }
-}
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
